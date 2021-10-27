8dc57e046c0f79323c3a8e7bb01468a6a8432c40
Mokienko <dmokienko@luxoft.com>
2021-10-27 08:57:25 +0300

    fix prettier

--- src/utils/nodes.ts
+++ src/utils/nodes.ts
@@ -1,43 +1,43 @@
 import styles from "../styles.module.css";
 
 export const createLinkNode = (url: string, isLast: boolean) => {
-  const link = document.createElement("a");
-  link.setAttribute("href", url);
-  link.setAttribute("target", "_blank");
-  link.setAttribute("contenteditable", "false");
-  // todo: add custom
-  link.className = styles.textFieldLink;
-  link.innerHTML = isLast ? url : `${url} `;
-  return link;
+    const link = document.createElement("a");
+    link.setAttribute("href", url);
+    link.setAttribute("target", "_blank");
+    link.setAttribute("contenteditable", "false");
+    // todo: add custom
+    link.className = styles.textFieldLink;
+    link.innerHTML = isLast ? url : `${url} `;
+    return link;
 };
 
 export const createTextNode = (text: string, isLast: boolean) => {
-  if (text === "\n") {
-    return document.createElement("br");
-  }
-  return document.createTextNode(isLast ? text : `${text} `);
+    if (text === "\n") {
+        return document.createElement("br");
+    }
+    return document.createTextNode(isLast ? text : `${text} `);
 };
 
 export const isInline = (tag: string) => {
-  return tag === "SPAN" || tag === "#text" || tag === "A";
+    return tag === "SPAN" || tag === "#text" || tag === "A";
 };
 
 export const getNodeText = (node: Node | HTMLElement) => {
-  if (node instanceof HTMLElement) {
-    return node.innerText || "";
-  }
-  return node.textContent || "";
+    if (node instanceof HTMLElement) {
+        return node.innerText || "";
+    }
+    return node.textContent || "";
 };
 
 export const createContentLineWrapper = (nodeName: string): HTMLElement => {
-  if (nodeName === "#text") {
-    return document.createElement("span");
-  }
-  const tag = nodeName.toLowerCase();
-  return document.createElement(tag);
+    if (nodeName === "#text") {
+        return document.createElement("span");
+    }
+    const tag = nodeName.toLowerCase();
+    return document.createElement(tag);
 };
 
 export const clearElement = (element: HTMLElement | null) => {
-  if (!element) return "";
-  element.innerHTML = "";
+    if (!element) return "";
+    element.innerHTML = "";
 };
